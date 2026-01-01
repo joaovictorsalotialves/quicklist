3d43a1477745e752e6e00ca4b477b98ebc70ae9c
João Victor <joaovictorsalotialves12@gmail.com>
2026-01-01 19:35:56 -0300

    feat: Criação da lógica para marcar e desmarcar um item da lista

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
-const form = document.getElementById('form')
-const inputNewItem = document.getElementById('new-item')
+const form = document.getElementById("form")
+const inputNewItem = document.getElementById("new-item")
 
-const list = document.getElementById('list-items')
+const list = document.getElementById("list-items")
 
-const remove_alert = document.getElementById('alert')
+const remove_alert = document.getElementById("alert")
 
 form.onsubmit = (event) => {
   event.preventDefault()
@@ -11,23 +11,23 @@ form.onsubmit = (event) => {
   const value = inputNewItem.value
 
   if (!value) {
-    alert('Por favor! Informe um valor para adicionar o item')
+    alert("Por favor! Informe um valor para adicionar o item")
     return
   }
 
   addNewItem(value)
 
-  inputNewItem.value = ''
+  inputNewItem.value = ""
 }
 
 function addNewItem(value) {
-  const li = document.createElement('li')
-  li.classList.add('item')
+  const li = document.createElement("li")
+  li.classList.add("item")
 
   li.innerHTML = `
     <div class="checkbox-wrapper">
       <div class="checkbox-image"></div>
-      <input type="checkbox">
+      <input class="checkbox" type="checkbox">
       <span>${value}</span>
     </div>
     <img class="icon-remove" src="assets/icons/delete.svg" alt="Excluir item ${value}">
@@ -36,22 +36,26 @@ function addNewItem(value) {
   list.appendChild(li)
 }
 
-list.addEventListener('click', function (event) {
+list.addEventListener("click", function (event) {
+  const item = event.target.closest(".item")
+  
   if (event.target.classList.contains("icon-remove")) {
-    const item = event.target.closest(".item")
     item.remove()
 
-    remove_alert.classList.remove('hidden')
+    remove_alert.classList.remove("hidden")
 
     setTimeout(() => {
-      remove_alert.classList.add('hidden')
+      remove_alert.classList.add("hidden")
     }, 15000)
+  }
+
+  if (event.target.classList.contains("checkbox")) {
+    item.classList.toggle('checked')
   }
 })
 
-remove_alert.addEventListener('click', function (event) {
-  console.log(event.target)
+remove_alert.addEventListener("click", function (event) {
   if (event.target.classList.contains("icone-close")) {
-    remove_alert.classList.add('hidden')
+    remove_alert.classList.add("hidden")
   }
 })
